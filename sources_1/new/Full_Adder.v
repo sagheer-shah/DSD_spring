605e472fbdec33bbd167dc34c6cb8723b16e379c
sagheer-shah <79632254+sagheer-shah@users.noreply.github.com>
2021-02-25 17:41:45 +0500

    Update Full_Adder.v

--- sources_1/new/Full_Adder.v
+++ sources_1/new/Full_Adder.v
@@ -1,24 +1,7 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/25/2021 03:21:43 PM
-// Design Name: 
-// Module Name: Full_Adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+
+//Auhtor: Sagheer Abbas Shah
 
 module Full_Adder(sum, c_out, a, b, c_in);
 //dimensions and size
