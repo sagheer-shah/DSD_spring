e675fffdc1bc5b727cf342fd3b5bf319c3866ef5
sagheer-shah <79632254+sagheer-shah@users.noreply.github.com>
2021-02-25 17:42:24 +0500

    Update Inverter.v

--- sources_1/new/Inverter.v
+++ sources_1/new/Inverter.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/25/2021 03:00:10 PM
-// Design Name: 
-// Module Name: Inverter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+//Author: Sagheer Abbas Shah
 
 
 module Inverter(A, A_not);
