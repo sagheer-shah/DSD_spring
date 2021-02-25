125132062bfb4d27bcbcf5c67ef854d4edca6f4c
sagheer-shah <79632254+sagheer-shah@users.noreply.github.com>
2021-02-25 18:27:47 +0500

    Update Parallel_Adder.v

--- sources_1/new/Parallel_Adder.v
+++ sources_1/new/Parallel_Adder.v
@@ -14,7 +14,7 @@ wire c1,c2,c3;
 //logic description
 Half_Adder HA0(sum[0], c1, a[0], b[0]);//HA instantiated
 Full_Adder FA0(sum[1], c2, a[1], b[1], c1);//FA instantiated
-Full_Adder FA1(sum[2], c3, a[2], b[1], c2);//FA instantiated
+  Full_Adder FA1(sum[2], c3, a[2], b[2], c2);//FA instantiated
 Full_Adder FA2(sum[3], c_out, a[3], b[3], c3);//FA instantiated
 
 endmodule
